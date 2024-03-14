6f8f14df4b20a7c05df3b4ba9bdd59d4a0df3d34
Latfiansya <latfiansyadzikri@gmail.com>
2024-03-14 16:06:29 +0700

    Output dan Looping

--- Insertion Sort.cpp
+++ Insertion Sort.cpp
@@ -1,12 +1,13 @@
-#include <isotream>
+#include <iostream>
 using namespace std;
 
 int arr[20];	//membuat array dengan panjang data 20
 int n;			//membuat variabel input n
+
 void input() {	//prosedur input
 	while (true) {
-		cout << "Masukkan jumlah data pada array : "; //membuat input jumlah elemen array
-		cin >> n;
+		 cout << "Masukkan jumlah data pada array : "; //membuat input jumlah elemen array
+		 cin >> n;
 
 		if (n <= 20) {
 			break;
@@ -15,4 +16,16 @@ void input() {	//prosedur input
 			cout << "\nArray yang anda masukan maksimal 20 elemen.\n"; //menampilkan pesan jika data lebih dari 20
 		}
 	}
+	cout << endl;								//membuat jarak baris program
+	cout << "=====================" << endl;	//membuat tampilan susunan data elemen array
+	cout << "Masukkan Elemen Array" << endl;
+	cout << "=====================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << " :";
+		cin >> arr[i];
+	}
+	
 }
+
+
